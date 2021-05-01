67618f95cfd9d023e4c1a6f968369c8b247c13c1
Sam <stwalters314@gmail.com>
2021-05-01 11:46:18 -0500

    html routes created and tested

---
+++ routes/htmlRoutes/html.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 
+const htmlRoutes = require('./routes/htmlRoutes/html');
+const apiRoutes = require('./routes/apiRoutes/api');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
